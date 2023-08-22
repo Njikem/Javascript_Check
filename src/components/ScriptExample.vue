5e7d32a33c0548e1581b09f05b52acc649aae24a
Njikem <123434988+Njikem@users.noreply.github.com>
2023-08-22 06:08:15 -0600

    vue

---
+++ src/components/ScriptExample.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+            tweets: ['mauby', 'bright', 'job'],
+            num_friends: [2,3,5]
+            logged_in: true
+
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
